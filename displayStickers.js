5e21b052a983284b948f70d8b9aed3b4ebf2cead
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-04-07 07:16:32 +0545

    show name and price of sticker

--- displayStickers.js
+++ displayStickers.js
@@ -1,12 +1,28 @@
 export default function displayStickers(stickers) {
     stickerGrid.innerHTML = '';
     stickers.forEach(sticker => {
+      //main div
       const stickerDiv = document.createElement('div');
       stickerDiv.classList.add('sticker');
+
+      //images
       const img = document.createElement('img');
       img.src = `images/${sticker.image}`; 
       img.alt = `${sticker.image}`;
       stickerDiv.appendChild(img);
+
+      //sticker name
+      const name = document.createElement('div');
+      name.classList.add('sticker-name');
+      name.textContent = `${sticker.name}`
+      stickerDiv.appendChild(name);
+
+      //sticker price
+      const price = document.createElement('div');
+      price.classList.add('sticker-price');
+      price.textContent = `${sticker.price}`
+      stickerDiv.appendChild(price);
+      
       stickerDiv.addEventListener('click', function() {
         navigateToStickerPage(sticker.id);
       });
