89f34f16d0237dbdcf8a5e18245d7d5927496c82
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-04-09 09:19:20 +0545

    login

---
+++ client/login.js
@@ -0,0 +1,33 @@
+// const apiUrl = "http://ashwink.com.np";
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('login-form');
+    const errorMessage = document.getElementById('error-message');
+  
+    loginForm.addEventListener('submit', async function(event) {
+      event.preventDefault();
+  
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+        token = btoa(username + ":" + password);
+      try {
+        const response = await fetch('https://stickers.ashwink.com.np/api/login', {
+          method: 'POST',
+          headers: {
+            Authorization : "Basic " + token,
+          },
+          body: JSON.stringify({ username, password })
+        });
+  
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.message || 'Failed to login');
+        }
+
+  
+        window.location.href = '/index.html'; // Redirect to dashboard on successful login
+      } catch (error) {
+        errorMessage.textContent = error.message;
+      }
+    });
+  });
+  
